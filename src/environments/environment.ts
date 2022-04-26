0147c33ec9c524706d3047e397ca5aefa52ed713
gohar <Omar.mostafa2010@science.bsu.edu.eg>
2022-04-27 00:24:30 +0200

    u

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
 
   apiUrl:'http://127.0.0.1:8000/api/super-admin',
-  apiUrlAdmin:'http://127.0.0.1:8000/api/admin'
+  apiUrlAdmin:'http://127.0.0.1:8000/api/admin',
 
   // apiURL: 'http://localhost:8000/api',
   images: 'http://localhost:8000',
