03fd652b3354c8022c7a40109509d7fd3f3b26d9
ogohar66 <99495688+ogohar66@users.noreply.github.com>
2022-04-27 02:25:29 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
 
   apiURL:'http://127.0.0.1:8000/api',
+  apiLogin:'http://127.0.0.1:8000/api/super-admin',
   apiAdminURL:'http://127.0.0.1:8000/api/admin',
   Images:'http://127.0.0.1:8000',
 
